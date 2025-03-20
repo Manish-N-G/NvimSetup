cd5509ac80823e6a55854b1a5d6e6e7c4d4ce865
Manish-N-G <136582364+Manish-N-G@users.noreply.github.com>
2025-03-21 00:43:34 +0100

    Add floatterm in comment

--- init.lua
+++ init.lua
@@ -380,6 +380,11 @@ require('lazy').setup({
       require("custom_main.nvim_tree_file")
     end
   },
+
+  -- TODOM: need to set up a good floating terminal
+  -- This creates a floating terminal
+  -- require 'custom.plugins.floaterm_file', -- floa_term block
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
